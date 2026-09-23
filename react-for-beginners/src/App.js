1ccc3cab7503a0ea23f93d23609752d5ecba317d
YJE <c0d1ngyje@gmail.com>
2026-09-23 17:31:46 +0900

    ✨ Feat: CRA 초기 설정 및 useEffect 학습 예제 구현

---
+++ react-for-beginners/src/App.js
@@ -0,0 +1,39 @@
+import { useState, useEffect } from "react";
+
+function App() {
+  const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
+  const onClick = () => {
+    setValue((prev) => prev + 1);
+  };
+  const onChange = (event) => {
+    setKeyword(event.target.value);
+  };
+  console.log("I run all the time");
+  useEffect(() => {
+    console.log("I run only once");
+  }, []);
+  useEffect(() => {
+    console.log("I run when 'keyword' changes");
+  }, [keyword]);
+  useEffect(() => {
+    console.log("I run when 'counter' changes");
+  }, [counter]);
+    useEffect(() => {
+      console.log("I run when 'keyword' & 'counter' change");
+    }, [keyword, counter]);
+  return (
+    <div>
+      <input
+        value={keyword}
+        onChange={onChange}
+        type="text"
+        placeholder="Search here"
+      ></input>
+      <h1>{counter}</h1>
+      <button onClick={onClick}>Click me</button>
+    </div>
+  );
+}
+
+export default App;
